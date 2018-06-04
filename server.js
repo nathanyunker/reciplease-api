186a9e4ec749cbfc062be0a3609b993014ffddcc
Nathan Yunker <nathanyunker@rocketmail.com>
2018-06-03 18:29:21 -0700

    trouble shooting access-control

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(function (req, res, next) {
 
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Origin', 'https://secret-wave-23471.herokuapp.com');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
